bb86f36a01ccc5da9d58e82c9c83febb0b9597d4
maxfrancis1 <max.francis2000@gmail.com>
2018-11-14 12:08:30 +0000

    Fixed the create function
    With help from Yannis :)

--- lib/space.rb
+++ lib/space.rb
@@ -5,11 +5,10 @@ class Space
 
   def self.create(name:, description:, price:)
 
-    x = User.get_user_id
-    user_id = x
+    x = User.get_user_id[0]
 
     connection = PG.connect(dbname: 'Makersbnb')
-    result = "INSERT INTO spaces (users_id, name, description, price) VALUES('#{user_id}', '#{name}', '#{description}', '#{price}') RETURNING users_id, name, description, price;"
+    result = "INSERT INTO spaces (users_id, name, description, price) VALUES('#{x}', '#{name}', '#{description}', '#{price}') RETURNING users_id, name, description, price;"
     connection.exec(result)
   end
 end
